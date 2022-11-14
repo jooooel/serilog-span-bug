f06cf47d8c432440ca05db12697388e5f0d38770
jooooel <joel.guedj@gmail.com>
2022-11-14 20:01:28 +0100

    Removed Serilog.Enrichers.Span
    Realized the offender is System.Diagnostics.DiagnosticsSource

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+
+Hello();
+
+public static partial class Program
+{
+    private static readonly ActivitySource MyActivitySource = new ActivitySource("MyCompany.MyProduct.MyLibrary");
+
+    public static void Hello()
+    {
+        using var activity = MyActivitySource.StartActivity("SayHello");
+        Console.WriteLine("Hello, World!");
+    }
+}
