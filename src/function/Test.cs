f858ed6dfef1c602b349bb86c3fcb1ff4d53e014
jooooel <joel.guedj@gmail.com>
2022-11-14 19:22:49 +0100

    Refactor

--- src/function/Test.cs
+++ src/function/Test.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace serilog_span_bug
+namespace function
 {
     public static class Test
     {
